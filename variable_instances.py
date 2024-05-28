59b8b2d9b30f47c83e0557a4902c6e97fcc79f38
Anmol chalise <anmolc650@gmail.com>
2024-05-28 10:12:15 +0545

    first commit and uplaod varibale_instnace

---
+++ variable_instances.py
@@ -0,0 +1,22 @@
+
+class student:
+    def __init__(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+        self.email=fname+lname+'@abc.com'
+
+    #class method .doesnot require any instance. class is passed as parameter
+    @classmethod
+    def salary(cls):
+        return 20000
+    
+    def full_name(self):
+        return self.fname+self.lname
+    
+    
+std1=student('Anmol','chalise')
+std2=student('Aakriti','chalise')
+
+print(std1.email)
+print(student.salary())
+print(std2.full_name())
